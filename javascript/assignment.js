b7c443cfd42ceeca7f17dba59582368231cd22e9
Emydan12 <akpabioemediong82@gmail.com>
2024-08-31 22:29:30 +0100

    Add files via upload

---
+++ javascript/assignment.js
@@ -0,0 +1,23 @@
+
+
+const carInStock = [
+    " ferrari",
+     "jeep",
+      "ram1500",
+       "bmw", 
+       "nissan",
+        "tesla",
+        "lexus",
+        "mercedes",
+        "toyota",
+        "honda"
+        ];
+
+        let customerChoice = promt("what car would you like to buy");
+
+        if( carInStock in carInStock (customerChoice)){
+            
+
+        }
+
+  
